84303f8a42217739d716129c50df04c5f495db00
Faris Siddiq <farissiddiqrp@gmail.com>
2025-05-22 02:09:49 +0700

    Kesalahan teknis

--- resources/views/lacakTiket.blade.php
+++ resources/views/lacakTiket.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets\css\style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets\css\stylePasien.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets\css\styleTiket.css') }}">>
+    <link rel="stylesheet" href="{{ asset('assets\css\styleTiket.css') }}">
     <style>
     </style>
 </head>
